543b24e7e510986d836146394f3220a76638bac7
Shrirang Edgaonkar <shrirange@gmail.com>
2021-08-11 21:21:41 +0530

    First successful implementation

--- src/main/java/com/example/demo/Experiment.java
+++ src/main/java/com/example/demo/Experiment.java
@@ -1,27 +1,50 @@
 package com.example.demo;
 
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-import org.hibernate.annotations.GeneratorType;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@Table(name = "Experiment")
 @EntityListeners(AuditingEntityListener.class)
 public class Experiment {
-	
+	 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long experimentId;
+	@Column(name = "experimentID")
+	private Integer experimentID;
 	
 	@Column(name = "experimentName")
 	private String experimentName;
+	
+	@Column(name = "experimentDate")
+	private Date experimentDate;
+	
+	@Column(name = "completed")
+	private Boolean completed;
+	
+	@Column(name = "comments")
+	private String comments;
+	
+	@Column(name = "reason")
+	private String reason;
+	
+	@Column(name = "instrument")
+	private String instrument;
+	
+	@ManyToOne
+	private Experiment experiment;
 	
 	@Column(name = "created_date", nullable = false, updatable = false)
     @CreatedDate
@@ -31,12 +54,12 @@ public class Experiment {
     @LastModifiedDate
     private long modifiedDate;
 
-	public long getExperimentId() {
-		return experimentId;
+	public Integer getExperimentID() {
+		return experimentID;
 	}
 
-	public void setExperimentId(long experimentId) {
-		this.experimentId = experimentId;
+	public void setExperimentID(Integer experimentID) {
+		this.experimentID = experimentID;
 	}
 
 	public String getExperimentName() {
@@ -47,22 +70,51 @@ public class Experiment {
 		this.experimentName = experimentName;
 	}
 
-	public long getCreatedDate() {
-		return createdDate;
+	public Date getExperimentDate() {
+		return experimentDate;
 	}
 
-	public void setCreatedDate(long createdDate) {
-		this.createdDate = createdDate;
+	public void setExperimentDate(Date experimentDate) {
+		this.experimentDate = experimentDate;
 	}
 
-	public long getModifiedDate() {
-		return modifiedDate;
+	public Boolean getCompleted() {
+		return completed;
 	}
 
-	public void setModifiedDate(long modifiedDate) {
-		this.modifiedDate = modifiedDate;
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
 	}
-    
-    
 
+	public Experiment getExperiment() {
+		return experiment;
+	}
+
+	public void setExperiment(Experiment experiment) {
+		this.experiment = experiment;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public String getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(String instrument) {
+		this.instrument = instrument;
+	}
 }
